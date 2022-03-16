bb2c64358f6159a5ecd372443fb648045af55393
David Risaro <drisaro@gmail.com>
2022-03-15 22:45:00 -0300

    Cambios agregando varios archivos

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ app.use(express.static('public'));
 const port = process.env.PORT || 3000;
 
 app.listen(port, ()=>{
-    console.log('Servidor funcionando');
+    console.log('Servidor arriba');
 });
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
 });
