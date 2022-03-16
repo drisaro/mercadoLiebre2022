c84c898bafa457ddbd86aa3284f309e3ed704bd3
David Risaro <drisaro@gmail.com>
2022-03-15 22:33:10 -0300

    Cambios en el puerto

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
+app.listen(port, ()=>{
     console.log('Servidor funcionando');
 });
 
